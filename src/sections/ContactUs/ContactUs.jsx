dc7417b77cc9b87990b8b2ad65503d3f4e85e96c
Alzin Mohamad Ghaith <35889862+alzin@users.noreply.github.com>
2023-07-03 03:03:17 +0000

    add implemenation for sending email

--- src/sections/ContactUs/ContactUs.jsx
+++ src/sections/ContactUs/ContactUs.jsx
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
 import "./ContactUs.css";
 
 import { Button } from "reactstrap";
 
 const ContactUs = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        "service_0orlrkc",
+        "template_fabncd4",
+        form.current,
+        "mqYufy-Epq6lV-Wdk"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          alert("Email sent successfully!");
+        },
+        (error) => {
+          console.log(error.text);
+          alert("An error occurred while sending the email.");
+        }
+      );
+    e.target.reset();
+  };
+
   return (
     <section id="contact-section">
       <div className="contact-section">
@@ -11,11 +37,11 @@ const ContactUs = () => {
         <div className="contact-details">
           <h2>Contact Us</h2>
           <p>Get in touch with us to discuss more...</p>
-          <form>
-            <input type="text" placeholder="Name" />
-            <input type="email" placeholder="Email" />
-            <textarea placeholder="Message"></textarea>
-            <Button size="lg" color="primary" outline type="submit">
+          <form ref={form} onSubmit={sendEmail}>
+            <input type="text" name="name" placeholder="Name" />
+            <input type="email" name="email" placeholder="Email" />
+            <textarea name="message" placeholder="Message"></textarea>
+            <Button color="success" size="lg" outline type="submit">
               Send Message
             </Button>
           </form>
