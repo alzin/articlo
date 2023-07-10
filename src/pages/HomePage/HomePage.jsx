f84b297f4969d261745486d2ce3bfed264ddf418
Alzin Mohamad Ghaith <35889862+alzin@users.noreply.github.com>
2023-07-10 13:34:43 +0900

    remove warnings. Add .env.local

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import QuestionInput from "../../components/QuestionInput/QuestionInput";
+
+const HomePage = ({ onSubmit }) => {
+  return <QuestionInput onSubmit={onSubmit} />;
+};
+
+export default HomePage;
