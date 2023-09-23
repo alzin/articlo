8bca4317212d07a716d3b437e5c23eeefa3fc93f
Mohamad Ghaith Alzin <ghaithalzein05@gmail.com>
2023-09-23 15:26:51 +0900

    support LP design

--- src/sections/PricingSection/PricingSection .jsx
+++ src/sections/PricingSection/PricingSection .jsx
@@ -14,39 +14,41 @@ const PricingSection = () => {
     <section id="pricing-section">
       <div className="pricing-container">
         <h1 className="pricing-heading">Our Plans</h1>
-        <div className="pricing-cards">
-          <div className="pricing-card">
-            <h2>Basic</h2>
-            <p>FREE</p>
-            <ul>
-              <li>5 Articles</li>
-              <li>Email Support</li>
-            </ul>
-            <button className="pricing-button" onClick={handleButtonClick}>
-              Access Now
-            </button>
-          </div>
-          <div className="pricing-card">
-            <h2>Pro</h2>
-            <p>FREE</p>
-            <ul>
-              <li>20 Articles</li>
-              <li>Priority Support</li>
-            </ul>
-            <button className="pricing-button" onClick={handleButtonClick}>
-              Access Now
-            </button>
-          </div>
-          <div className="pricing-card">
-            <h2>Enterprise</h2>
-            <p>Custom Pricing</p>
-            <ul>
-              <li>Unlimited Articles</li>
-              <li>24/7 Support</li>
-            </ul>
-            <a href="#contact-section" className="pricing-button">
-              Contact Us
-            </a>
+        <div className="pricing-grid">
+          <div className="pricing-cards">
+            <div className="pricing-card">
+              <h2>Basic</h2>
+              <p>FREE</p>
+              <ul>
+                <li>5 Articles</li>
+                <li>Email Support</li>
+              </ul>
+              <button className="pricing-button" onClick={handleButtonClick}>
+                Access Now
+              </button>
+            </div>
+            <div className="pricing-card">
+              <h2>Pro</h2>
+              <p>FREE</p>
+              <ul>
+                <li>20 Articles</li>
+                <li>Priority Support</li>
+              </ul>
+              <button className="pricing-button" onClick={handleButtonClick}>
+                Access Now
+              </button>
+            </div>
+            <div className="pricing-card">
+              <h2>Enterprise</h2>
+              <p>Custom Pricing</p>
+              <ul>
+                <li>Unlimited Articles</li>
+                <li>24/7 Support</li>
+              </ul>
+              <a href="#contact-section" className="pricing-button">
+                Contact Us
+              </a>
+            </div>
           </div>
         </div>
       </div>
