757c27a8c8c26e94737f3b296754c49c977c2f93
Mohamad Ghaith Alzin <ghaithalzein05@gmail.com>
2023-09-23 20:03:49 +0900

    edit design

--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -67,67 +67,65 @@ function Auth() {
   return (
     <div>
       <Navbar />
-      <div className="auth-wrapper">
-        <div className="auth-content">
-          <img src={AuthImage} alt="Auth" className="auth-image" />
-          <div className="auth-container">
-            <h2 className="auth-title">Get Started</h2>
-            <div className="auth-tabs">
-              <Tab
-                activeTab={activeTab}
-                onTabChange={handleTabChange}
-                tabs={[
-                  { id: "login", label: "Login" },
-                  { id: "signup", label: "Signup" },
-                ]}
-              />
-            </div>
-            <div className="auth-form">
-              {activeTab === "login" ? (
-                <form onSubmit={handleLoginSubmit}>
-                  <input
-                    type="email"
-                    name="email"
-                    placeholder="Email"
-                    value={loginData.email}
-                    onChange={handleLoginInputChange}
-                  />
-                  <input
-                    type="password"
-                    name="password"
-                    placeholder="Password"
-                    value={loginData.password}
-                    onChange={handleLoginInputChange}
-                  />
-                  <button type="submit">Login</button>
-                </form>
-              ) : (
-                <form onSubmit={handleSignupSubmit}>
-                  <input
-                    type="text"
-                    name="username"
-                    placeholder="Full Name"
-                    value={signupData.username}
-                    onChange={handleSignupInputChange}
-                  />
-                  <input
-                    type="email"
-                    name="email"
-                    placeholder="Email"
-                    value={signupData.email}
-                    onChange={handleSignupInputChange}
-                  />
-                  <input
-                    type="password"
-                    name="password"
-                    placeholder="Password"
-                    value={signupData.password}
-                    onChange={handleSignupInputChange}
-                  />
-                  <button type="submit">Sign up</button>
-                </form>
-              )}
-            </div>
+      <div className="auth-content">
+        <img src={AuthImage} alt="Auth" className="auth-image" />
+        <div className="auth-container">
+          <h2 className="auth-title">Get Started</h2>
+          <div className="auth-tabs">
+            <Tab
+              activeTab={activeTab}
+              onTabChange={handleTabChange}
+              tabs={[
+                { id: "login", label: "Login" },
+                { id: "signup", label: "Signup" },
+              ]}
+            />
+          </div>
+          <div className="auth-form">
+            {activeTab === "login" ? (
+              <form onSubmit={handleLoginSubmit}>
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="Email"
+                  value={loginData.email}
+                  onChange={handleLoginInputChange}
+                />
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  value={loginData.password}
+                  onChange={handleLoginInputChange}
+                />
+                <button type="submit">Login</button>
+              </form>
+            ) : (
+              <form onSubmit={handleSignupSubmit}>
+                <input
+                  type="text"
+                  name="username"
+                  placeholder="Full Name"
+                  value={signupData.username}
+                  onChange={handleSignupInputChange}
+                />
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="Email"
+                  value={signupData.email}
+                  onChange={handleSignupInputChange}
+                />
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  value={signupData.password}
+                  onChange={handleSignupInputChange}
+                />
+                <button type="submit">Sign up</button>
+              </form>
+            )}
           </div>
         </div>
       </div>
