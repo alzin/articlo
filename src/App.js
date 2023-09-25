0f98a2492be1640c9f25d6f1e504458b05130651
Mohamad Ghaith Alzin <ghaithalzein05@gmail.com>
2023-09-25 14:03:36 +0900

    redirect to home if users already logged in

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate  } from "react-router-dom";
 
 import MainContainer from "./pages/MainContainer/MainContainer.jsx";
 import LandingPage from "./pages/LandingPage/LandingPage.jsx";
@@ -7,12 +7,33 @@ import UserDashboard from "./components/UserDashboard/UserDashboard.jsx";
 import PrivateRoute from "./PrivateRoute.js";
 import Auth from "./pages/Auth/Auth.jsx";
 
+function LandingPageWrapper() {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    return <Navigate to="/home" />;
+  }
+
+  return <LandingPage />;
+}
+
+function AuthWrapper() {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    return <Navigate to="/home" />;
+  }
+
+  return <Auth />;
+}
+
+
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/auth" element={<Auth />} />
-        <Route path="/" element={<LandingPage />} />
+        <Route path="/auth" element={<AuthWrapper />} />
+        <Route path="/" element={<LandingPageWrapper />} />
         <Route exact path="/" element={<PrivateRoute />}>
           <Route path="/home" element={<MainContainer />} />
           <Route path="/articles" element={<UserDashboard />} />
